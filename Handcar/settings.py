c1653c03c7c1ac9487fce2ff03c93a3a76de9fda
yadhukrishnaofficial <yadhukrish605@gmail.com>
2025-05-28 16:53:24 +0530

    Removed STATICFILES_DIRS since static folder not used

--- Handcar/settings.py
+++ Handcar/settings.py
@@ -152,7 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
